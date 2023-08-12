ac86e497d36fc59aea2e932c92b1947e2b039df1
saroareee <saroareee@gmail.com>
2023-08-12 17:21:32 +0600

    Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#FBD784",
+        optional: "#000000",
+        offwhite: "#F4F5F5",
+        sectioncolor: "#EEF4FA",
+      },
+      fontFamily: {
+        sans: ["Open Sans"],
+        man: ["Manrope"],
+      },
+      fontSize: {
+        font64: "64px",
+        font19: "19px",
+      },
+    },
   },
   plugins: [],
 };
